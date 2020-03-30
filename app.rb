2900676beb5f669d115e2f989eb79372b14ff369
asuthamm <asuthamm@gmail.com>
2020-03-29 22:17:36 -0400

    refresh

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        erb :user_input
+    end
+
+    post '/PigLatinizer'
+        #model
+        pgl = PigLatinizer.new
+
+        #response
+        erb :PigLatinizer
+
+    end
 end
